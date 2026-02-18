e670ee2a1c2a311ae9480c401ac0cfdf76a40654
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2026-02-19 00:29:12 +0530

    chore: error handler to catch errors

---
+++ backend/middlewares/errorHandler.js
@@ -0,0 +1,18 @@
+const errorHandler = (err, req, res, next) => {
+    console.error(err);
+  
+    if (err.statusCode) {
+      return res.status(err.statusCode).json({
+        success: false,
+        message: err.message
+      });
+    }
+  
+    return res.status(500).json({
+      success: false,
+      message: 'Internal Server Error'
+    });
+  };
+  
+  module.exports = errorHandler;
+  
